21ee902504a55252a2858fdc31fa02ddfdc1c690
HamzaHosein101 <hamzahosein8@gmail.com>
2025-10-24 00:18:14 -0400

    Admin page

--- app/src/main/java/com/example/travelpractice/data/DefaultSeed.kt
+++ app/src/main/java/com/example/travelpractice/data/DefaultSeed.kt
@@ -5,7 +5,9 @@ data class DefaultCategory(val title: String, val items: List<String>)
 val defaultSeed = listOf(
     DefaultCategory("Toiletries", listOf("Toothbrush", "Toothpaste", "Deodorant", "Shampoo")),
     DefaultCategory("Clothes", listOf("T-Shirts", "Jeans", "Underwear", "Socks")),
-    DefaultCategory("Electronics", listOf("Phone charger", "Power bank", "Headphones"))
+    DefaultCategory("Electronics", listOf("Phone charger", "Power bank", "Headphones")),
+    DefaultCategory("Documents", listOf("Passport", "Driver’s License", "Boarding Pass")),
+    DefaultCategory("Travel", listOf("Travel Pillow", "Eye Mask", "Blanket"))
 )
 
 
